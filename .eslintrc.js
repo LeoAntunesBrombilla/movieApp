6a1a4c2dae9ba83fc678218bd2394d3939fbce4d
LeoAntunesBrombilla <antunes.b.leonardo@gmail.com>
2022-08-13 17:03:54 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  jsxSingleQuote: true,
+  quoteProps: 'as-needed',
+  singleQuote: true,
+  semi: true,
+  printWidth: 100,
+  trailingComma: 'es5',
+};
